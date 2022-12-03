7e2074db784b6a6c7ac760a269756d21b8ee49cd
quackercrumbs <calvinquach@gmail.com>
2022-12-03 14:48:46 -0500

    2022-day3/clojure: p2

--- 2022/day3/clojure/dev.clj
+++ 2022/day3/clojure/dev.clj
@@ -10,16 +10,16 @@
       x))
 
 #_(do
-  (clojure.repl/apropos "split")
-  (clojure.repl/doc clojure.core/split-at)
-  (->> (clojure.core/split-at (quot (count "zz") 2) "zaa")
-       (mapv set)
-       (apply clojure.set/intersection)
-       (mapv (fn [c] (if (Character/isUpperCase c)
-                       (+ 27 (- (int c) (int \A)))
-                       (inc (- (int c) (int \a)))))))
-  (int \a)
-  (Character/isUpperCase \A))
+    (clojure.repl/apropos "split")
+    (clojure.repl/doc clojure.core/split-at)
+    (->> (clojure.core/split-at (quot (count "zz") 2) "zaa")
+         (mapv set)
+         (apply clojure.set/intersection)
+         (mapv (fn [c] (if (Character/isUpperCase c)
+                         (+ 27 (- (int c) (int \A)))
+                         (inc (- (int c) (int \a)))))))
+    (int \a)
+    (Character/isUpperCase \A))
 
 (defn problem-1
   []
@@ -38,3 +38,38 @@
          (apply +))))
 (problem-1)
 ; 8053
+
+#_(do
+  (clojure.repl/apropos "partition")
+  (clojure.repl/doc partition)
+  (->> ["AA" "Ab" "Ac" "BB" "Bb" "Bc"]
+       (partition 3)
+       (mapv (fn [group] (->>
+                          (mapv set group)
+                          (apply set/intersection))))
+       (apply concat)
+       (mapv (fn [c] (if (Character/isUpperCase c)
+                       (+ 27 (- (int c) (int \A)))
+                       (+ 1 (- (int c) (int \a))))))
+       (apply +)))
+
+(defn problem-2
+  []
+  (let [lines (with-open [file (io/reader "/home/calvinq/projects/aoc/2022/day3/input.txt")]
+                (doall (line-seq file)))]
+    (->> lines
+         ;; group of 3 lines
+         (partition 3)
+         ;; find common element in the three lines
+         (mapv (fn [group] (->>
+                            (mapv set group)
+                            (apply set/intersection))))
+         ;; Combine [#{\A} #{\B}] into one list
+         (apply concat)
+         ;; convert to priority
+         (mapv (fn [c] (if (Character/isUpperCase c)
+                         (+ 27 (- (int c) (int \A)))
+                         (+ 1 (- (int c) (int \a))))))
+         ;; find sum
+         (apply +))))
+(problem-2) ; 2425
