0546760e7d79fb73711661855980293745e48495
quackercrumbs <calvinquach@gmail.com>
2022-12-04 15:20:29 -0500

    2022-day4/rust: p2

--- 2022/day4/rust/src/main.rs
+++ 2022/day4/rust/src/main.rs
@@ -15,6 +15,9 @@ fn main() -> io::Result<()> {
     let result = problem_1(lines.clone());
     println!("problem 1: {:?}", result);
 
+    let result = problem_2(lines.clone());
+    println!("problem 2: {:?}", result);
+
     Ok(())
 }
 
@@ -70,5 +73,57 @@ mod tests {
         ]
         .into();
         assert_eq!(2, problem_1(input));
+    }
+}
+
+fn range_overlaps(range_1: &ops::RangeInclusive<u64>, range_2: &ops::RangeInclusive<u64>) -> bool {
+    return (range_1.contains(&range_2.start()) || range_1.contains(&range_2.end()))
+        || (range_2.contains(&range_1.start()) || range_2.contains(&range_1.end()));
+}
+
+fn problem_2(lines: Vec<String>) -> u64 {
+    let answer = lines
+        .into_iter()
+        .map(|team_string| {
+            let team_ranges = team_string
+                .split(",")
+                .map(|range| {
+                    let range = range
+                        .split("-")
+                        .map(|n| n.parse::<u64>().unwrap())
+                        .collect::<Vec<u64>>();
+                    let low = range.get(0).unwrap().to_owned();
+                    let high = range.get(1).unwrap().to_owned();
+                    ops::RangeInclusive::new(low, high)
+                })
+                .collect::<Vec<ops::RangeInclusive<u64>>>();
+
+            let range_1 = team_ranges.get(0).unwrap();
+            let range_2 = team_ranges.get(1).unwrap();
+            range_overlaps(range_1, range_2)
+        })
+        .filter(|result| result == &true)
+        .count();
+
+    return answer as u64;
+}
+
+#[cfg(test)]
+mod tests_problem2 {
+
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let input: Vec<String> = vec![
+            String::from("2-4,6-8"),
+            String::from("2-3,4-5"),
+            String::from("5-7,7-9"),
+            String::from("2-8,3-7"),
+            String::from("6-6,4-6"),
+            String::from("2-6,4-8"),
+        ]
+        .into();
+        assert_eq!(4, problem_2(input));
     }
 }
